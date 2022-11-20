00f305bd26076fe7f345496f391b5a1f07448975
DineshTs-coder <85272477+DineshTs-coder@users.noreply.github.com>
2022-11-20 13:24:01 +0530

    Add files via upload

---
+++ quizdatabase/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Divided by zero is?",
+    answer: "Run time Error",
+    options: [
+      "Syntax Error",
+      "Run time Error",
+      "System Error",
+      "Output Error"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What can be used to create the objects?",
+    answer: "class",
+    options: [
+      "objects",
+      "module",
+      "data item",
+      "class"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A Program must never be?",
+    answer: "long",
+    options: [
+      "Understandable",
+      "accurate",
+      "long",
+      "portable"
+    ]
+  },
+    {
+    numb: 4,
+    question: " An array is the collection of?",
+    answer: "Both",
+    options: [
+      "Common Namee",
+      "Common Datatype",
+      "Both",
+      "None"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Debugging is the technique for?",
+    answer: "remove errors from code",
+    options: [
+      "remove errors from code",
+      "edit software",
+      "developing software",
+      "All of the above"
+    ]
+  },
+   {
+   numb: 6,
+   question: " What is not true about java language?",
+    answer: "It is platform dependent",
+     options: [
+      "It is platform independent",
+	  "It is platform dependent",
+      "It is object oriented",
+      "It is robust and secure"
+    ]
+  },
+  {
+   numb: 7,
+   question: " Java uses?",
+    answer: "Assembler",
+     options: [
+      "Compiler",
+	  "Interpreter",
+      "Both 1 and 2",
+      "Assembler"
+    ]
+  },
+   {
+   numb: 8,
+   question: " For compiling process java used?",
+    answer: "javac",
+     options: [
+      "javac",
+      "javacomp",
+      "java",
+      "jdb"
+    ]
+  },
+  {
+   numb: 9,
+   question: "For interpreting process java used?",
+    answer: "java",
+     options: [
+      "javac",
+      "javacomp",
+      "java",
+      "jdb"
+    ]
+  },
+  {
+   numb: 10,
+   question: " For viewing web application java uses?",
+    answer: "applet viewer",
+     options: [
+      "applet viewer",
+      "javacomp",
+      "jdb",
+      "javac"
+    ]
+  },
+];
